fc0ee8698111b3d05e3c69a60de697d409bf5119
RJ Bowen <rjbowen@udel.edu>
2017-12-20 21:44:59 -0500

    Added the main file

--- PrimeTriples/PrimeTriples/PrimeTriplesMain.cs
+++ PrimeTriples/PrimeTriples/PrimeTriplesMain.cs
@@ -10,6 +10,35 @@ namespace PrimeTriples
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello world!");
+        }
+
+        static HashSet<double> getPrimeBooleans(int n)
+        {
+            HashSet<int> NoPrime = new HashSet<int>();
+
+            long count = 0;
+
+            for (int x = 2; x < n; x++)
+            {
+                for (int y = x * 2; y < n; y = y + x)
+                {
+                    if (!NoPrime.Contains(y))
+                    {
+                        NoPrime.Add(y);
+                    }
+                }
+
+                for (int z = 2; z < n; z++)
+                {
+                    if (!NoPrime.Contains(z))
+                    {
+                        Console.WriteLine(z);
+                        count = count + z;
+                    }
+                }
+                Console.WriteLine("Sum is: " + count);
+            }
 
         }
     }
