4523404dec867bebfb692b1600b03b9e16f17a27
RJ Bowen <rjbowen@udel.edu>
2017-12-26 14:18:52 -0500

    Progress on the strong probable prime test using base two. Added running times next to function declarations

--- PrimeTriples/PrimeTriples/PrimeTriplesMain.cs
+++ PrimeTriples/PrimeTriples/PrimeTriplesMain.cs
@@ -17,7 +17,35 @@ namespace PrimeTriples
 
     static class PrimeCalculator
     {
+        //Uses a strong probable prime test with base two
+        static bool IsStrongProbablePrime(int n)
+        {
+            int s = 0;
+            int d = n;
 
+            for (int testS = 0; testS < Math.Log(n, 2); testS++) //O(log(n))
+            {
+                for (int testD = 1; testD < n; testD += 2) //O(n) (check to see what limit must be here)
+                {
+                    if (Math.Pow(2,testS) * testD == n-1) //n = d*2^s + 1
+                    {
+                        s = testS;
+                        d = testD;
+                    }
+                }
+            }
+
+            if ((Math.Pow(2, d) - 1) % n == 0) //O(1)
+            {
+                return true;
+            }
+
+            for (int r = 0; r < s; r++) //O(s - 1) == O(log(n))
+            {
+
+            }
+
+        }
     }
 
     class TriangleRow
@@ -30,23 +58,23 @@ namespace PrimeTriples
             return "startNumber: " + startNumber + "\nendNumber " + endNumber;
         }
 
-        public TriangleRow(ulong n)
+        public TriangleRow(ulong n) //O(2n) == O(n)
         {
             startNumber = GetRowStartNumber(n);
             endNumber = GetRowEndNumber(n);
         }
 
-        private ulong GetRowStartNumber(ulong n)
+        private ulong GetRowStartNumber(ulong n) //O(n)
         {
             return CalculatePrefixSum(n) + 1;
         }
 
-        private ulong GetRowEndNumber(ulong n)
+        private ulong GetRowEndNumber(ulong n) //O(n)
         {
             return CalculatePrefixSum(n + 1);
         }
 
-        private ulong CalculatePrefixSum(ulong n)
+        private ulong CalculatePrefixSum(ulong n) //O(n)
         {
             ulong prefixSum = 0;
 
