e7f8788caca945ba7985c41b2208ffe6a5790261
Richard Bowen <richard.bowen@listrak.com>
2017-12-28 13:24:22 -0500

    added a jacobi number generator algorithm

--- PrimeTriples/PrimeTriples/PrimeTriplesMain.cs
+++ PrimeTriples/PrimeTriples/PrimeTriplesMain.cs
@@ -25,7 +25,7 @@ namespace PrimeTriples
             {
                 for (double testD = 1; testD < n; testD += 2) //O(n) (check to see what limit must be here)
                 {
-                    if (n - 1 == Math.Pow(2,testS) * testD) //n = d*2^s + 1
+                    if (n - 1 == Math.Pow(2, testS) * testD) //n = d*2^s + 1
                     {
                         s = testS;
                         d = testD;
@@ -40,7 +40,7 @@ namespace PrimeTriples
 
             for (double r = 0; r < s; r++) //O(s - 1) == O(log(n))
             {
-                if (Modulo((Math.Pow(2, d*Math.Pow(2,r)) + 1), n) == 0)
+                if (Modulo((Math.Pow(2, d * Math.Pow(2, r)) + 1), n) == 0)
                 {
                     return true;
                 }
@@ -71,48 +71,86 @@ namespace PrimeTriples
             }
         }
 
-        static double CalculateJacobiSymbol(double D, double n)
+        static double CalculateJacobiSymbol(double a, double b)
         {
-            
-        }
-    }
-
-    class TriangleRow
-    {
-        ulong startNumber;
-        ulong endNumber;
-
-        public override string ToString()
-        {
-            return "startNumber: " + startNumber + "\nendNumber " + endNumber;
-        }
-
-        public TriangleRow(ulong n) //O(2n) == O(n)
-        {
-            startNumber = GetRowStartNumber(n);
-            endNumber = GetRowEndNumber(n);
-        }
-
-        private ulong GetRowStartNumber(ulong n) //O(n)
-        {
-            return CalculatePrefixSum(n) + 1;
-        }
-
-        private ulong GetRowEndNumber(ulong n) //O(n)
-        {
-            return CalculatePrefixSum(n + 1);
-        }
-
-        private ulong CalculatePrefixSum(ulong n) //O(n)
-        {
-            ulong prefixSum = 0;
-
-            for (ulong i = 0; i <= n; i++)
+            if (b <= 0 || (Modulo(b, 2)) == 0)
             {
-                prefixSum += i;
+                return 0;
             }
-            return prefixSum;
+            double j = 1;
+            if (a < 0)
+            {
+                a = -a;
+                if (Modulo(b, 4) == 3)
+                {
+                    j = -j;
+                }
+            }
+
+            while (a != 0)
+            {
+                while ((Modulo(a, 2) == 0))
+                {
+                    a = a / 2;
+                    if ((Modulo(b, 8)) == 3 || (Modulo(b, 8)) == 5)
+                    {
+                        j = -j;
+                    }
+                }
+
+                double temp = a;
+                a = b;
+                b = temp;
+
+                if ((Modulo(a, 4)) == 3 && (Modulo(b, 4)) == 3) j = -j;
+                a = Modulo(a,b);
+            }
+            if (b == 1)
+            {
+                return (j);
+            }
+            else
+            {
+                return (0);
+            }
         }
     }
+}
 
+class TriangleRow
+{
+    ulong startNumber;
+    ulong endNumber;
+
+    public override string ToString()
+    {
+        return "startNumber: " + startNumber + "\nendNumber " + endNumber;
+    }
+
+    public TriangleRow(ulong n) //O(2n) == O(n)
+    {
+        startNumber = GetRowStartNumber(n);
+        endNumber = GetRowEndNumber(n);
+    }
+
+    private ulong GetRowStartNumber(ulong n) //O(n)
+    {
+        return CalculatePrefixSum(n) + 1;
+    }
+
+    private ulong GetRowEndNumber(ulong n) //O(n)
+    {
+        return CalculatePrefixSum(n + 1);
+    }
+
+    private ulong CalculatePrefixSum(ulong n) //O(n)
+    {
+        ulong prefixSum = 0;
+
+        for (ulong i = 0; i <= n; i++)
+        {
+            prefixSum += i;
+        }
+        return prefixSum;
+    }
 }
